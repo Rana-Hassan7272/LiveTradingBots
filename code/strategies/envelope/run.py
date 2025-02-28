ae17d6b63c032f9bdee083154ab089f71e0501d2
Rana-Hassan7272 <152691833+Rana-Hassan7272@users.noreply.github.com>
2025-03-01 00:44:57 +0500

    Update run.py

--- code/strategies/envelope/run.py
+++ code/strategies/envelope/run.py
@@ -31,8 +31,8 @@ if strategy == 'scalping':
         'entry_ema_fast': 9,
         'entry_ema_slow': 21,
         'rsi_period': 14,
-        'rsi_overbought': 60,
-        'rsi_oversold': 30,
+        'rsi_overbought': 80,
+        'rsi_oversold': 65,
         # Exit Parameters
         'trailing_stop_distance': 6,  # in USD
         'take_profit_ratio': 1.5,     # Ratio relative to stop distance
